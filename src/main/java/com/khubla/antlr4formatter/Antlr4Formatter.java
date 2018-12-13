aa4adbdc947d0e5dd7be9b02c5d7edded8280d2b
Matthias Balke <matthias.balke@googlemail.com>
2018-12-13 17:36:25 +0100

    extracted formatting into an own class
    updated libs
    refactored build process to generated sources to target

--- src/main/java/com/khubla/antlr4formatter/Antlr4Formatter.java
+++ src/main/java/com/khubla/antlr4formatter/Antlr4Formatter.java
@@ -17,16 +17,6 @@
  */
 package com.khubla.antlr4formatter;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.antlr.parser.antlr4.ANTLRv4Lexer;
 import org.antlr.parser.antlr4.ANTLRv4Parser;
@@ -34,35 +24,43 @@ import org.antlr.parser.antlr4.ANTLRv4Parser.GrammarSpecContext;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Tom Everett
  */
 public class Antlr4Formatter {
-   /**
-    * input option
-    */
-   private static final String INPUT_OPTION = "input";
-   /**
-    * output option
-    */
-   private static final String OUTPUT_OPTION = "output";
-   /**
-    * output option
-    */
-   private static final String DIR_OPTION = "dir";
+   
+   private static final Logger LOG = LoggerFactory.getLogger(Antlr4Formatter.class);
 
-   /**
-    * format an input file
-    */
-   private static void format(InputStream inputStream, OutputStream outputStream) throws Exception {
+
+   public static String format(String string) {
+      try {
+         if (null != string) {
+            final ANTLRv4Lexer lexer = new ANTLRv4Lexer(new ANTLRInputStream(string));
+            final CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+            final ANTLRv4Parser parser = new ANTLRv4Parser(commonTokenStream);
+            final GrammarSpecContext grammarSpecContext = parser.grammarSpec();
+            StringWriter writer = new StringWriter();
+            ParseTreeWalker.DEFAULT.walk(new Antlr4ParseTreeListenerImpl(writer, commonTokenStream), grammarSpecContext);
+            return writer.toString();
+         } else {
+            return "";
+         }
+      } catch (final Exception e) {
+         throw new RuntimeException("Exception reading and parsing file", e);
+      }
+   }
+
+   public static void format(InputStream inputStream, OutputStream outputStream) throws Exception {
       try {
          if (null != inputStream) {
             final Reader reader = new InputStreamReader(inputStream, "UTF-8");
@@ -70,7 +68,11 @@ public class Antlr4Formatter {
             final CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
             final ANTLRv4Parser parser = new ANTLRv4Parser(commonTokenStream);
             final GrammarSpecContext grammarSpecContext = parser.grammarSpec();
-            ParseTreeWalker.DEFAULT.walk(new Antlr4ParseTreeListenerImpl(new PrintStream(outputStream), commonTokenStream), grammarSpecContext);
+
+            try(OutputStreamWriter writer = new OutputStreamWriter(outputStream)) {
+               ParseTreeWalker.DEFAULT.walk(new Antlr4ParseTreeListenerImpl(writer, commonTokenStream), grammarSpecContext);
+               writer.flush();
+            }
          } else {
             throw new IllegalArgumentException();
          }
@@ -79,128 +81,63 @@ public class Antlr4Formatter {
       }
    }
 
-   static void formatDirectory(String inputDirOption) throws Exception {
-      List<String> files = new ArrayList<String>();
-      files = getFiles(inputDirOption, files, ".g4");
+   public static void formatDirectory(String inputDirOption) throws Exception {
+      List<String> files = new ArrayList<>();
+      files = listFilesFromDirectory(inputDirOption, files, ".g4");
       for (final String filename : files) {
          final File file = new File(filename);
-         formatFile(file, file);
+         formatSingleFile(file, file);
       }
    }
 
-   /*
-    * format a single file
-    */
-   static void formatFile(File inputFile, File outputFile) throws Exception {
-      /*
-       * filenames
-       */
-      if (true == inputFile.exists()) {
-         /*
-          * temp file
-          */
+   public static void formatSingleFile(File inputFile, File outputFile) throws Exception {
+
+      if (inputFile.exists()) {
+
          final File tempFile = File.createTempFile(inputFile.getName(), ".g4");
-         /*
-          * show the filename
-          */
-         System.out.println("Formatting: " + inputFile.getName());
-         /*
-          * format
-          */
+
+         LOG.info("Formatting: {}", inputFile.getName());
+
          format(new FileInputStream(inputFile), new FileOutputStream(tempFile));
-         /*
-          * copy file
-          */
-         FileUtils.copyFile(tempFile, outputFile);
-         tempFile.delete();
+
+         Files.copy(tempFile.toPath(), outputFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+         Files.delete(tempFile.toPath());
       } else {
          throw new Exception("Unable to find: '" + inputFile + "'");
       }
    }
 
-   static void formatSingleFile(String inputFilename, String outputFilename) throws Exception {
-      /*
-       * files
-       */
+   public static void formatSingleFile(String inputFilename, String outputFilename) throws Exception {
       final File inputFile = new File(inputFilename);
       File outputFile;
+      
       if (null == outputFilename) {
-         /*
-          * no output specified, use a temp file
-          */
          outputFile = inputFile;
       } else {
          outputFile = new File(outputFilename);
       }
-      /*
-       * format
-       */
-      formatFile(inputFile, outputFile);
+
+      formatSingleFile(inputFile, outputFile);
    }
 
-   /**
-    * get a list of all files in a directory
-    */
-   private static List<String> getFiles(String dir, List<String> files, String filter) throws Exception {
+   private static List<String> listFilesFromDirectory(String dir, List<String> files, String filter) {
       final File file = new File(dir);
       final String[] list = file.list();
       if (null != list) {
-         for (int i = 0; i < list.length; i++) {
-            final String fileName = dir + (dir.endsWith("/") ? "" : "/") + list[i];
-            final File f2 = new File(fileName);
-            if (false == f2.isHidden()) {
-               if (f2.isDirectory()) {
-                  getFiles(fileName + "/", files, filter);
-               } else {
-                  if (fileName.endsWith(filter)) {
-                     files.add(fileName);
-                  }
-               }
+        for (String s : list) {
+          final String fileName = dir + (dir.endsWith("/") ? "" : "/") + s;
+          final File f2 = new File(fileName);
+          if (!f2.isHidden()) {
+            if (f2.isDirectory()) {
+              listFilesFromDirectory(fileName + "/", files, filter);
+            } else {
+              if (fileName.endsWith(filter)) {
+                files.add(fileName);
+              }
             }
-         }
+          }
+        }
       }
       return files;
-   }
-
-   public static void main(String[] args) {
-      try {
-         System.out.println("khubla.com Antlr4 Formatter");
-         /*
-          * options
-          */
-         final Options options = new Options();
-         final Option o1 = Option.builder().argName(INPUT_OPTION).longOpt(INPUT_OPTION).type(String.class).hasArg().required(false).desc("input file").build();
-         options.addOption(o1);
-         final Option o2 = Option.builder().argName(OUTPUT_OPTION).longOpt(OUTPUT_OPTION).type(String.class).hasArg().required(false).desc("output file").build();
-         options.addOption(o2);
-         final Option o3 = Option.builder().argName(DIR_OPTION).longOpt(DIR_OPTION).type(String.class).hasArg().required(false).desc("input dir").build();
-         options.addOption(o3);
-         /*
-          * parse
-          */
-         final CommandLineParser parser = new DefaultParser();
-         CommandLine cmd = null;
-         try {
-            cmd = parser.parse(options, args);
-         } catch (final Exception e) {
-            e.printStackTrace();
-            final HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("posix", options);
-            System.exit(0);
-         }
-         /*
-          * get the file
-          */
-         final String inputFilename = cmd.getOptionValue(INPUT_OPTION);
-         final String outputFilename = cmd.getOptionValue(OUTPUT_OPTION);
-         final String inputDirOption = cmd.getOptionValue(DIR_OPTION);
-         if (null == inputDirOption) {
-            formatSingleFile(inputFilename, outputFilename);
-         } else {
-            formatDirectory(inputDirOption);
-         }
-      } catch (final Exception e) {
-         e.printStackTrace();
-      }
    }
 }
